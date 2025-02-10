86e415ac7ec0eec800ebd4bc58dd1abf029f134c
DanielBradac <daniel.bradac@email.cz>
2025-02-10 16:23:00 +0100

    table of content

--- src/routes/print/+page.svelte
+++ src/routes/print/+page.svelte
@@ -18,6 +18,7 @@
         } else {
             throw Error("Couldn't read selected catgegories.");
         }
+
         window.print();
     });
 
@@ -26,78 +27,117 @@
     }
 </script>
 
-<button onclick={onPrintButton}>Vytisknout</button>
+<div class="printPage">
+    <h1>Kontrolní a zkušební plán</h1>
+    <h2>(DÍLČÍ ČÁST PLÁNU KVALITY STAVBY)</h2>
+    <h3>OBSAH:</h3>
 
-<p>Table of contents TODO</p>
+    <table>
+        <tbody>
+            {#each selectedCategories as category}
+                <tr>
+                    <td>{category.id}.</td>
+                    <td>{category.name}</td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
+<button onclick={onPrintButton}>Vytisknout</button>
 
 <div bind:this={tablesContainer}>
     {#each selectedCategories as category}
-        <table class="table-pagebreak" border="1">
-            <thead>
-                <tr>
-                    <th colspan={tableHeaders.length}>
-                        {category.id}. {category.name}
-                    </th>
-                </tr>
-                <tr>
-                    <th colspan={mainHeaderCount}>Název firmy: STAZEPO a.s.</th>
-                    <th colspan={tableHeaders.length - mainHeaderCount}>
-                        Technické podklady: platné ČSN normy
-                    </th>
-                </tr>
-                <tr>
-                    {#each tableHeaders as header}
-                        <th>{header}</th>
-                    {/each}
-                </tr>
-            </thead>
-
-            <tbody>
-                {#each category.sections as section}
+        <div class="printPage">
+            <table class="categoryTable" border="1">
+                <thead>
                     <tr>
-                        <td>{section.id}</td>
-                        <td>{section.inspectionName}</td>
-                        <td>{section.regulation}</td>
-                        <td>{section.frequency}</td>
-                        <td>{section.performer}</td>
-                        <td>{section.record}</td>
-                        {#each Array.from( { length: emptyColsIndexes.length }, ) as _}
-                            <td></td>
+                        <th class="categoryTH" colspan={tableHeaders.length}>
+                            {category.id}. {category.name}
+                        </th>
+                    </tr>
+                    <tr>
+                        <th class="categoryTH" colspan={mainHeaderCount}
+                            >Název firmy: STAZEPO a.s.</th
+                        >
+                        <th
+                            class="categoryTH"
+                            colspan={tableHeaders.length - mainHeaderCount}
+                        >
+                            Technické podklady: platné ČSN normy
+                        </th>
+                    </tr>
+                    <tr>
+                        {#each tableHeaders as header}
+                            <th>{header}</th>
                         {/each}
                     </tr>
-                {/each}
-            </tbody>
-        </table>
+                </thead>
+
+                <tbody>
+                    {#each category.sections as section}
+                        <tr>
+                            <td class="categoryTD">{section.id}</td>
+                            <td class="categoryTD">{section.inspectionName}</td>
+                            <td class="categoryTD">{section.regulation}</td>
+                            <td class="categoryTD">{section.frequency}</td>
+                            <td class="categoryTD">{section.performer}</td>
+                            <td class="categoryTD">{section.record}</td>
+                            {#each Array.from( { length: emptyColsIndexes.length }, ) as _}
+                                <td class="categoryTD"></td>
+                            {/each}
+                        </tr>
+                    {/each}
+                </tbody>
+            </table>
+            Zápis jen v případě zjištění neshody.
+        </div>
     {/each}
 </div>
 
 <style>
-    table {
+    .categoryTable {
         width: 100%; /* Uniform table width */
-        table-layout: fixed;
         border-collapse: collapse;
         font-family: "Times New Roman", Times, serif;
         text-wrap: auto;
     }
 
-    th,
-    td {
+    .categoryTH,
+    .categoryTD {
         text-align: left;
-        padding: 8px;
-        border: 1px solid #ddd;
+        padding: 4px;
+        border: 1px solid #000000;
+    }
+
+    h1 {
+        text-align: center;
+        font-size: 2em;
+    }
+
+    h2 {
+        text-align: center;
+        font-size: 1.5em;
+    }
+
+    h3 {
+        font-size: 1.2em;
+    }
+
+    .printPage {
+        margin-bottom: 2em;
+    }
+
+    button {
+        margin-bottom: 2em;
     }
 
     @media print {
+        .printPage {
+            page-break-after: always;
+        }
+
         button {
             display: none;
-        }
-
-        table {
-            page-break-after: always;
-        }
-
-        p {
-            page-break-after: always;
         }
 
         @page {
